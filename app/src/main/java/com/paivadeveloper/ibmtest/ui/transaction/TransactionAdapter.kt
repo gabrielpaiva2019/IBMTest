4a68ace5c8c4951c31b9b8dc28cd4a82dbfc1513
Gabriel <cgpaiva@icloud.com>
2020-03-10 15:47:21 -0300

    Popula textos formatados na recyclerview, ajusta padding da recyclerview

--- app/src/main/java/com/paivadeveloper/ibmtest/ui/transaction/TransactionAdapter.kt
+++ app/src/main/java/com/paivadeveloper/ibmtest/ui/transaction/TransactionAdapter.kt
@@ -6,6 +6,10 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.paivadeveloper.ibmtest.R
 import com.paivadeveloper.ibmtest.model.Statement
+import com.paivadeveloper.ibmtest.util.DateUtil
+import kotlinx.android.synthetic.main.item_statement.view.*
+import java.text.NumberFormat
+
 
 class TransactionAdapter(private var listStatements: MutableList<Statement.StatementItem>) :
     RecyclerView.Adapter<TransactionAdapter.ViewHolder>() {
@@ -17,17 +21,26 @@ class TransactionAdapter(private var listStatements: MutableList<Statement.State
     }
 
     override fun getItemCount(): Int {
-      return listStatements.size
+        return listStatements.size
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        var statement = listStatements[position]
+        val valueFormatted = NumberFormat.getCurrencyInstance().format(statement.value)
+        val dateFormatted = DateUtil().getFormattedDate(statement.date)
+
+        holder.textViewTransactionValue.text = valueFormatted
+        holder.textViewTransactionDate.text = dateFormatted
+        holder.textViewTransactionDescription.text = statement.desc
+        holder.textViewTransactionType.text = statement.title
     }
 
 
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var textViewTransactionType = itemView.textViewTransactionType
+        var textViewTransactionDescription = itemView.textViewTransactionDescription
+        var textViewTransactionValue = itemView.textViewTransactionValue
+        var textViewTransactionDate = itemView.textViewTransactionDate
 
     }
-
-
-
 }
---
+++ app/src/main/java/com/paivadeveloper/ibmtest/util/DateUtil.kt
@@ -0,0 +1,15 @@
+package com.paivadeveloper.ibmtest.util
+
+import java.text.SimpleDateFormat
+import java.util.*
+
+class DateUtil {
+
+    fun getFormattedDate(date: String): String {
+        val formatToDate = SimpleDateFormat("yyyy-MM-dd")
+        val date: Date = formatToDate.parse(date)
+        val formatDateToString = SimpleDateFormat("dd/MM/yyyy")
+        return formatDateToString.format(date)
+    }
+
+}
